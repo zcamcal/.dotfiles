9ea1bfb8ec7257c3f30f38197190d0a39fddda0d
zcamcal <188593322+zcamcal@users.noreply.github.com>
2024-11-24 00:38:00 -0300

    feat: update.sh deleted replaced by symlink, ln waybar

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
 ln -sf ~/.dotfiles/sway ~/.config/sway
+ln -sf ~/.dotfiles/waybar ~/.config/waybar
 ln -sf ~/.dotfiles/tmux ~/.config/tmux
--- update.sh
+++
@@ -1,4 +0,0 @@
-cp -r ~/.config/foot ./
-cp -r ~/.config/nvim ./
-cp -r ~/.config/waybar ./
-cp -r ~/.config/sway ./
