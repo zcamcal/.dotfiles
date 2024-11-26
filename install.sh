12a132bb7fac82ca6a7230861640c4176ba2aa99
zcamcal <188593322+zcamcal@users.noreply.github.com>
2024-11-25 22:07:54 -0300

    doc: there's so much to learn yet ;)

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 ln -sf ~/.dotfiles/sway ~/.config/sway
 ln -sf ~/.dotfiles/waybar ~/.config/waybar
 ln -sf ~/.dotfiles/tmux ~/.config/tmux
+
+## TODO: learn more about stow and use it because ln if symlink exists will append other symlink ;(
